ffa09ee6ddfbf7761b51b3186142fbfc15af7e9c
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 10:19:27 +0000

    Vote update

--- trunk/json/votes/2010/ma-all.js
+++ trunk/json/votes/2010/ma-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 06:17 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 06:19 AM","totals":
 {"name":"Massachusetts","fix":
 {"Barnstable":"Barnstable Town","Manchester":"Manchester-by-the-Sea"},
 "precincts":
