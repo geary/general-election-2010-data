f19b3a6ce032c4618cff919c832793e7bdfae760
judebritto@google.com <judebritto@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 13:32:19 +0000

    Vote update

--- trunk/json/votes/2010/il-all.js
+++ trunk/json/votes/2010/il-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 09:26 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 09:32 AM","totals":
 {"name":"Illinois","fix":
 {"JoDaviess":"Jo Daviess","DeWitt":"De Witt","LaSalle":"La Salle"},
 "precincts":
--- trunk/json/votes/2010/ms-all.js
+++ trunk/json/votes/2010/ms-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 09:26 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 09:32 AM","totals":
 {"name":"Mississippi","fix":
 {"Jeff Davis":"Jefferson Davis"},
 "precincts":
