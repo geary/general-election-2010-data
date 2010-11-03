d9e6967d1ba39f671a84daaef1bbeb42320b58a6
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 02:39:12 +0000

    Vote update

--- trunk/json/votes/2010/ks-all.js
+++ trunk/json/votes/2010/ks-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 02,2010 10:36 PM","totals":
+votesReady( {"dttime":"Nov 02,2010 10:39 PM","totals":
 {"name":"Kansas","precincts":
 {"total":1321,"reporting":448},
 "races":
--- trunk/json/votes/2010/ma-all.js
+++ trunk/json/votes/2010/ma-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 02,2010 10:36 PM","totals":
+votesReady( {"dttime":"Nov 02,2010 10:39 PM","totals":
 {"name":"Massachusetts","fix":
 {"Barnstable":"Barnstable Town","Manchester":"Manchester-by-the-Sea"},
 "precincts":
