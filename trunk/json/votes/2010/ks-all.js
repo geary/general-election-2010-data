67c0d2388c700db7309dd4f97ed3718d01b7fc92
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 04:29:14 +0000

    Vote update

--- trunk/json/votes/2010/ks-all.js
+++ trunk/json/votes/2010/ks-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 12:26 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:29 AM","totals":
 {"name":"Kansas","precincts":
 {"total":1321,"reporting":1016},
 "races":
