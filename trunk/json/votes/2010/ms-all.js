b78e2657373892141fe9924e98cc96dfdbf7b44a
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 07:56:15 +0000

    Vote update

--- trunk/json/votes/2010/ms-all.js
+++ trunk/json/votes/2010/ms-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 03:53 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 03:56 AM","totals":
 {"name":"Mississippi","fix":
 {"Jeff Davis":"Jefferson Davis"},
 "precincts":
--- trunk/json/votes/2010/oh-all.js
+++ trunk/json/votes/2010/oh-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 03:53 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 03:56 AM","totals":
 {"races":
 {"U.S. House":
 {"11":
