32fa63c5518b65812b1878bdbc01eba32c050b10
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 05:32:00 +0000

    Vote update

--- json/votes/2010/tx-all.js
+++ json/votes/2010/tx-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 01:29 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 01:31 AM","totals":
 {"name":"Texas","fix":
 {"De Witt":"DeWitt","La Vaca":"Lavaca"},
 "precincts":
--- json/votes/2010/vt-all.js
+++ json/votes/2010/vt-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 01:29 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 01:31 AM","totals":
 {"name":"Vermont","precincts":
 {"total":260,"reporting":222},
 "races":
