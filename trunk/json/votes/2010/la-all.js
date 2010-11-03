e4e5e12821b5d569706d0fc3435d7d120c3b8204
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 10:35:21 +0000

    Vote update

--- trunk/json/votes/2010/la-all.js
+++ trunk/json/votes/2010/la-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 06:32 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 06:35 AM","totals":
 {"name":"Louisiana","fix":
 {"DeSoto":"De Soto","Jeff Davis":"Jefferson Davis","LaSalle":"La Salle"},
 "precincts":
