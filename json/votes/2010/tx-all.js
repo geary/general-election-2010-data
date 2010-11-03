7edc0b3e087781ba98b8cf92eaeb68bcd6a3d7f8
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 11:11:45 +0000

    Vote update

--- json/votes/2010/tx-all.js
+++ json/votes/2010/tx-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 07:09 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 07:11 AM","totals":
 {"name":"Texas","fix":
 {"De Witt":"DeWitt","La Vaca":"Lavaca"},
 "precincts":
