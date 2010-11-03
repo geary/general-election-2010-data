67d7dab567ad3b2fc6300f364a84bad4d7562d35
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 08:35:34 +0000

    Vote update

--- trunk/json/votes/2010/nm-all.js
+++ trunk/json/votes/2010/nm-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 04:33 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 04:35 AM","totals":
 {"name":"New Mexico","fix":
 {"DeBaca":"De Baca"},
 "precincts":
--- trunk/json/votes/2010/tx-all.js
+++ trunk/json/votes/2010/tx-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 04:33 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 04:35 AM","totals":
 {"name":"Texas","fix":
 {"De Witt":"DeWitt","La Vaca":"Lavaca"},
 "precincts":
