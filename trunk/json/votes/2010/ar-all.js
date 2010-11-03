c46b833e081a19f54e53ce8f8c9f2ba54edb4fc5
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 07:58:50 +0000

    Vote update

--- trunk/json/votes/2010/ar-all.js
+++ trunk/json/votes/2010/ar-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 03:56 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 03:58 AM","totals":
 {"name":"Arkansas","fix":
 {"Saint Francis":"St. Francis"},
 "precincts":
