4235381ba14251446a11615c7f6260192f2ada33
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 04:08:10 +0000

    Vote update

--- trunk/json/votes/2010/ks-all.js
+++ trunk/json/votes/2010/ks-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 12:05 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:08 AM","totals":
 {"name":"Kansas","precincts":
 {"total":1321,"reporting":862},
 "races":
--- trunk/json/votes/2010/la-all.js
+++ trunk/json/votes/2010/la-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 12:05 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:08 AM","totals":
 {"name":"Louisiana","fix":
 {"DeSoto":"De Soto","Jeff Davis":"Jefferson Davis","LaSalle":"La Salle"},
 "precincts":
