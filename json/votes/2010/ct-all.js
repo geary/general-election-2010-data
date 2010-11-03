ebae0edaaec9934f6858cd53a75aa93e5a3f00e2
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 04:48:22 +0000

    Vote update

--- json/votes/2010/ct-all.js
+++ json/votes/2010/ct-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 12:45 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:48 AM","totals":
 {"name":"Connecticut","precincts":
 {"total":156,"reporting":127},
 "races":
