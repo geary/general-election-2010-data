2f8eaa0b0fe1dc7cdaa35007e7c8163a0ff569cb
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 06:26:20 +0000

    Vote update

--- json/votes/2010/me-all.js
+++ json/votes/2010/me-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 02:24 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 02:26 AM","totals":
 {"name":"Maine","fix":
 {},
 "precincts":
--- json/votes/2010/nm-all.js
+++ json/votes/2010/nm-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 02:24 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 02:26 AM","totals":
 {"name":"New Mexico","fix":
 {"DeBaca":"De Baca"},
 "precincts":
