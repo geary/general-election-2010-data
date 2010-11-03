09c02009dc0c72f5ffec05d622a0ba9953713af1
judebritto@google.com <judebritto@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 14:27:28 +0000

    Vote update

--- json/votes/2010/ks-all.js
+++ json/votes/2010/ks-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 10:21 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 10:27 AM","totals":
 {"name":"Kansas","precincts":
 {"total":1321,"reporting":1321},
 "races":
