345856a7495404e666c2d52eebe794f0e2a30a96
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 04:19:44 +0000

    Vote update

--- trunk/json/votes/2010/il-all.js
+++ trunk/json/votes/2010/il-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 12:17 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:19 AM","totals":
 {"name":"Illinois","fix":
 {"JoDaviess":"Jo Daviess","DeWitt":"De Witt","LaSalle":"La Salle"},
 "precincts":
--- trunk/json/votes/2010/vt-all.js
+++ trunk/json/votes/2010/vt-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 12:17 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:19 AM","totals":
 {"name":"Vermont","precincts":
 {"total":260,"reporting":202},
 "races":
