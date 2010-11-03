bfc5a3d7185b7138e36109cf456d636e02be0de2
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 08:54:51 +0000

    Vote update

--- json/votes/2010/fl-all.js
+++ json/votes/2010/fl-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 04:52 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 04:54 AM","totals":
 {"races":
 {"U.S. House":
 {"24":
--- json/votes/2010/la-all.js
+++ json/votes/2010/la-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 04:52 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 04:54 AM","totals":
 {"name":"Louisiana","fix":
 {"DeSoto":"De Soto","Jeff Davis":"Jefferson Davis","LaSalle":"La Salle"},
 "precincts":
