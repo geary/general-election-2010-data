1ea2faa6b40dcc4d392bcfb4079215fbd8696509
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 04:36:21 +0000

    Vote update

--- json/votes/2010/dc-all.js
+++ json/votes/2010/dc-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 12:33 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:36 AM","totals":
 {"abbr":"DC","name":"District of Columbia"},
 "state":"DC","candidates":
 {},
--- json/votes/2010/ma-all.js
+++ json/votes/2010/ma-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 12:33 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:36 AM","totals":
 {"name":"Massachusetts","fix":
 {"Barnstable":"Barnstable Town","Manchester":"Manchester-by-the-Sea"},
 "precincts":
