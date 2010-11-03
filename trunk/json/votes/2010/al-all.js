19df306dc9a18294b1d16bba63782890a57c0a1f
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 08:16:19 +0000

    Vote update

--- trunk/json/votes/2010/al-all.js
+++ trunk/json/votes/2010/al-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 04:13 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 04:16 AM","totals":
 {"races":
 {"U.S. House":
 {"1":
--- trunk/json/votes/2010/ok-all.js
+++ trunk/json/votes/2010/ok-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 04:13 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 04:16 AM","totals":
 {"name":"Oklahoma","fix":
 {"LeFlore":"Le Flore"},
 "precincts":
