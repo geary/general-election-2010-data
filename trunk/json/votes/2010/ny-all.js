0e8f66c460f21fd9fc180fcf3d9a967d97ba9de2
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 05:54:48 +0000

    Vote update

--- trunk/json/votes/2010/ny-all.js
+++ trunk/json/votes/2010/ny-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 01:52 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 01:54 AM","totals":
 {"name":"New York","fix":
 {"Brooklyn":"Kings","Staten Island":"Richmond","Saint Lawrence":"St. Lawrence","Manhattan":"New York"},
 "precincts":
