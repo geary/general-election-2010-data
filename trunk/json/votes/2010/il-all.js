f7b2c6a82aa0a00f20eaff4bf5ce972499daccef
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 10:47:04 +0000

    Vote update

--- trunk/json/votes/2010/il-all.js
+++ trunk/json/votes/2010/il-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 06:44 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 06:47 AM","totals":
 {"name":"Illinois","fix":
 {"JoDaviess":"Jo Daviess","DeWitt":"De Witt","LaSalle":"La Salle"},
 "precincts":
