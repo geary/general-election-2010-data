c215ab0600468c28f64f8b2ad54b49947d4d7790
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 05:15:21 +0000

    Vote update

--- trunk/json/votes/2010/dc-all.js
+++ trunk/json/votes/2010/dc-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 01:12 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 01:15 AM","totals":
 {"abbr":"DC","name":"District of Columbia"},
 "state":"DC","candidates":
 {},
