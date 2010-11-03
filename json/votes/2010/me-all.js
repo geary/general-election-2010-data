53a8ace2db5a88942eb113fd71221e0a57eaa5da
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 07:25:24 +0000

    Vote update

--- json/votes/2010/me-all.js
+++ json/votes/2010/me-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 03:23 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 03:25 AM","totals":
 {"name":"Maine","fix":
 {},
 "precincts":
--- json/votes/2010/vt-all.js
+++ json/votes/2010/vt-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 03:23 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 03:25 AM","totals":
 {"name":"Vermont","precincts":
 {"total":260,"reporting":232},
 "races":
