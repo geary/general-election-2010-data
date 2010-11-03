3ab788a9de141221b5f9778167bbad908ca8914b
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 10:54:10 +0000

    Vote update

--- json/votes/2010/ks-all.js
+++ json/votes/2010/ks-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 06:51 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 06:54 AM","totals":
 {"name":"Kansas","precincts":
 {"total":1321,"reporting":1321},
 "races":
--- json/votes/2010/nm-all.js
+++ json/votes/2010/nm-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 06:51 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 06:54 AM","totals":
 {"name":"New Mexico","fix":
 {"DeBaca":"De Baca"},
 "precincts":
