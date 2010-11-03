ecf716ab96b3bc2ebc9bc3c0647706fa4ccb374a
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 06:38:56 +0000

    Vote update

--- json/votes/2010/mn-all.js
+++ json/votes/2010/mn-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 02:36 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 02:38 AM","totals":
 {"name":"Minnesota","fix":
 {"Lac Qui Parle":"Lac qui Parle"},
 "precincts":
