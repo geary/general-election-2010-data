41dda2e1277be6e62be5267d32c736c84996173c
judebritto@google.com <judebritto@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 16:04:08 +0000

    Vote update

--- json/votes/2010/ma-all.js
+++ json/votes/2010/ma-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 11:58 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:04 PM","totals":
 {"name":"Massachusetts","fix":
 {"Barnstable":"Barnstable Town","Manchester":"Manchester-by-the-Sea"},
 "precincts":
