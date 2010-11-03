09dc292cde8005d6da5c2749118ecb4462f71404
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 11:14:00 +0000

    Vote update

--- trunk/json/votes/2010/vt-all.js
+++ trunk/json/votes/2010/vt-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 07:11 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 07:13 AM","totals":
 {"name":"Vermont","precincts":
 {"total":260,"reporting":232},
 "races":
