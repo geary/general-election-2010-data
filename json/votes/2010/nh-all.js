a0331a07c944791bf337f6af86ed4bccd283d26f
judebritto@google.com <judebritto@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 17:33:50 +0000

    Vote update

--- json/votes/2010/nh-all.js
+++ json/votes/2010/nh-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 01:27 PM","totals":
+votesReady( {"dttime":"Nov 03,2010 01:33 PM","totals":
 {"name":"New Hampshire","fix":
 {"":"Atkinson and Gilmanton Academy"},
 "precincts":
