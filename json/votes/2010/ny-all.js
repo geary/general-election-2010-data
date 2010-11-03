052e386a0f8e41b88edeb71f1c7a21fcbdf467ab
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 04:01:06 +0000

    Vote update

--- json/votes/2010/ny-all.js
+++ json/votes/2010/ny-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 02,2010 11:58 PM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:01 AM","totals":
 {"name":"New York","fix":
 {"Brooklyn":"Kings","Staten Island":"Richmond","Saint Lawrence":"St. Lawrence","Manhattan":"New York"},
 "precincts":
--- json/votes/2010/tx-all.js
+++ json/votes/2010/tx-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 02,2010 11:58 PM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:01 AM","totals":
 {"name":"Texas","fix":
 {"De Witt":"DeWitt","La Vaca":"Lavaca"},
 "precincts":
