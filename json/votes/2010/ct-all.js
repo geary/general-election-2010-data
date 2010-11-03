6cd4843bc60cccd3f8cb100bb430515ce0ab8280
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 08:42:47 +0000

    Vote update

--- json/votes/2010/ct-all.js
+++ json/votes/2010/ct-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 04:40 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 04:42 AM","totals":
 {"name":"Connecticut","precincts":
 {"total":156,"reporting":154},
 "races":
--- json/votes/2010/tx-all.js
+++ json/votes/2010/tx-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 04:40 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 04:42 AM","totals":
 {"name":"Texas","fix":
 {"De Witt":"DeWitt","La Vaca":"Lavaca"},
 "precincts":
