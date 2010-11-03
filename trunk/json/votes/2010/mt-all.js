4c8c80f3e2a478487fddd60ef31c8513a97eac03
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 03:49:23 +0000

    Vote update

--- trunk/json/votes/2010/mt-all.js
+++ trunk/json/votes/2010/mt-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 02,2010 11:46 PM","totals":
+votesReady( {"dttime":"Nov 02,2010 11:49 PM","totals":
 {"name":"Montana","fix":
 {"Lewis & Clark":"Lewis and Clark"},
 "precincts":
