1ae3d2e81f6c9225560821bb91951dee6af030e0
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 11:36:27 +0000

    Vote update

--- json/votes/2010/co-all.js
+++ json/votes/2010/co-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 07:31 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 07:36 AM","totals":
 {"races":
 {"U.S. House":
 {"1":
--- json/votes/2010/mo-all.js
+++ json/votes/2010/mo-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 07:31 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 07:36 AM","totals":
 {"name":"Missouri","fix":
 {"LaClede":"Laclede"},
 "precincts":
