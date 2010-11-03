f3d04ad89ab74c69658a2d61c210c97d7f0f642f
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 08:23:27 +0000

    Vote update

--- json/votes/2010/ma-all.js
+++ json/votes/2010/ma-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 04:21 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 04:23 AM","totals":
 {"name":"Massachusetts","fix":
 {"Barnstable":"Barnstable Town","Manchester":"Manchester-by-the-Sea"},
 "precincts":
--- json/votes/2010/mt-all.js
+++ json/votes/2010/mt-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 04:21 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 04:23 AM","totals":
 {"name":"Montana","fix":
 {"Lewis & Clark":"Lewis and Clark"},
 "precincts":
