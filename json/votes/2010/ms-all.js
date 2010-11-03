067cee342d126951408d9f0911c940c38c437714
rajivb@google.com <rajivb@google.com@f3eac3fe-5478-7463-c79f-52354a643273>
2010-11-03 04:19:44 +0000

    Vote update

--- json/votes/2010/ms-all.js
+++ json/votes/2010/ms-all.js
@@ -1,4 +1,4 @@
-votesReady( {"dttime":"Nov 03,2010 12:17 AM","totals":
+votesReady( {"dttime":"Nov 03,2010 12:19 AM","totals":
 {"name":"Mississippi","fix":
 {"Jeff Davis":"Jefferson Davis"},
 "precincts":
